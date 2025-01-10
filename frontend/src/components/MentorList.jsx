1ee3e5a550509ef5aa2b45d789f80394e82ea7d7
rohitprofc <rohitprofc@gmail.com>
2025-01-10 19:31:33 +0530

    frontend boilerplate setup

---
+++ frontend/src/components/MentorList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MentorList = () => {
+  return (
+    <div>
+      <h2>Mentor List</h2>
+      {/* Add Mentor listing */}
+    </div>
+  );
+};
+
+export default MentorList;
